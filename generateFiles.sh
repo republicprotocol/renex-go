f7c37c73f8cd1987fa0b7a762b0866ec8256479a
Jaz Gulati <jaskaran.g15@gmail.com>
2018-07-20 10:52:23 +1000

    bash: removed branch specification from script

--- generateFiles.sh
+++ generateFiles.sh
@@ -3,16 +3,13 @@
 MODULE_FOLDER="modules/renex-js"
 UI_FOLDER="ui"
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
 # Add modules
 if [ -d $MODULE_FOLDER ]; then
     cd $MODULE_FOLDER
-    git checkout $BRANCH
     git pull
     cd ../..
 else
-    git clone -b $BRANCH https://github.com/republicprotocol/renex-js.git modules/renex-js
+    git clone https://github.com/republicprotocol/renex-js.git modules/renex-js
 fi
 
 # Remove the old build folder
