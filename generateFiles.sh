4252788b5ecf6d813d56c8139def7f06ceda5de9
Jaz Gulati <jaskaran.g15@gmail.com>
2018-07-20 11:23:39 +1000

    bash: added --branch flag to script

--- generateFiles.sh
+++ generateFiles.sh
@@ -3,13 +3,23 @@
 MODULE_FOLDER="modules/renex-js"
 UI_FOLDER="ui"
 
+if [ "$1" != "--branch" ] || [ "$2" = "" ]; then
+    echo "Please specify a branch to build using the --branch flag"
+    exit 1
+else
+    echo "--branch is specified"
+fi
+
+BRANCH=$2
+
 # Add modules
 if [ -d $MODULE_FOLDER ]; then
     cd $MODULE_FOLDER
-    git pull
+    git checkout $BRANCH
+    git pull origin $BRANCH
     cd ../..
 else
-    git clone https://github.com/republicprotocol/renex-js.git modules/renex-js
+    git clone -b $BRANCH https://github.com/republicprotocol/renex-js.git modules/renex-js
 fi
 
 # Remove the old build folder
