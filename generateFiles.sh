cf0aa22492f492ce6721ea37dd06791178648822
Jaz Gulati <jaskaran.g15@gmail.com>
2018-07-20 10:50:21 +1000

    bash: updated script to build current branch

--- generateFiles.sh
+++ generateFiles.sh
@@ -3,21 +3,24 @@
 MODULE_FOLDER="modules/renex-js"
 UI_FOLDER="ui"
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 # Add modules
-if [ -d "$MODULE_FOLDER" ]; then
-    cd "$MODULE_FOLDER"
+if [ -d $MODULE_FOLDER ]; then
+    cd $MODULE_FOLDER
+    git checkout $BRANCH
     git pull
     cd ../..
 else
-    git clone https://github.com/republicprotocol/renex-js.git modules/renex-js
+    git clone -b $BRANCH https://github.com/republicprotocol/renex-js.git modules/renex-js
 fi
 
 # Remove the old build folder
 rm -rf $UI_FOLDER
 
 # Build UI
-cd "$MODULE_FOLDER"
+cd $MODULE_FOLDER
 npm install
 npm run build
-mv build "../../$UI_FOLDER"
+mv build ../../$UI_FOLDER
 cd ../..
