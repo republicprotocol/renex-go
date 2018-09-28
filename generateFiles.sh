c325fcbfb78ae7fe5c92698d8ba8ccaa7c80a143
Negaex <gh@negaex.com>
2018-09-28 11:30:56 +1000

    Updated generateFiles script

--- generateFiles.sh
+++ generateFiles.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Print commands as they are executed
-set -x
-
 # Exit if any command fails
 set -e
 
@@ -12,12 +9,52 @@ RENEX_MODULE_FOLDER="$BASE_FOLDER/modules/renex-js"
 SDK_MODULE_FOLDER="$BASE_FOLDER/modules/renex-sdk-ts"
 UI_FOLDER="$BASE_FOLDER/ui"
 
-if [ "$1" != "--branch" ] || [ "$2" = "" ]; then
-    echo "Please specify a branch to build using the --branch flag"
+RESET='\033[0m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+
+usage() {
+    echo "Please specify a network to deploy using the --network flag"
+    echo ""
+    echo -e "  Available networks: ${BLUE}mainnet${RESET}, ${PURPLE}testnet${RESET}, ${CYAN}nightly${RESET}"
+    echo ""
+    exit 1
+}
+
+if [ "$1" != "--network" ] || [ "$2" = "" ]; then
+    usage
+fi
+
+NETWORK=$2
+
+NOVERIFY=false
+if [ "$3" == "--no-verify" ]; then
+    NOVERIFY=true
+fi
+
+
+if [ "$NETWORK" == "mainnet" ]; then
+    BRANCH="master"
+    COLOR="${BLUE}"
+elif [ "$NETWORK" == "testnet" ]; then
+    BRANCH="develop"
+    COLOR="${PURPLE}"
+elif [ "$NETWORK" == "nightly" ]; then
+    BRANCH="nightly"
+    COLOR="${CYAN}"
+else
+    usage
     exit 1
 fi
 
-BRANCH=$2
+echo -e "\nDeploying ${GREEN}renex-js:${BRANCH}${RESET} with ${GREEN}renex-sdk-ts:${BRANCH}${RESET} to ${COLOR}${NETWORK}${RESET}...\n\n"
+
+# Print commands as they are executed
+set -x
 
 # Add modules
 if [ -d $RENEX_MODULE_FOLDER ]; then
@@ -41,11 +78,22 @@ else
     git clone -b $BRANCH git@github.com:republicprotocol/renex-sdk-ts.git "$SDK_MODULE_FOLDER"
 fi
 
-# Get latest commit hash
+# Get latest renex-js commit hash and author
 cd "$RENEX_MODULE_FOLDER"
-LATEST_COMMIT="`git rev-parse HEAD`"
-cd $BASE_FOLDER
-echo -n "$LATEST_COMMIT" > env/latest_commit.txt
+LATEST_RENEX_COMMIT="`git rev-parse --short HEAD`"
+LATEST_RENEX_AUTHOR="`git --no-pager  log --format='%aN <%aE>' HEAD^!`"
+cd "$BASE_FOLDER"
+
+# Get latest renex-sdk-ts commit hash
+cd "$SDK_MODULE_FOLDER"
+LATEST_SDK_COMMIT="`git rev-parse --short HEAD`"
+LATEST_SDK_AUTHOR="`git --no-pager  log --format='%aN <%aE>' HEAD^!`"
+cd "$BASE_FOLDER"
+
+COMBINED_HASH="$LATEST_RENEX_COMMIT//$LATEST_SDK_COMMIT"
+
+echo -n "${COMBINED_HASH}" > env/latest_commit.txt
+
 
 # Remove the old build folder
 rm -rf $UI_FOLDER
@@ -63,3 +111,39 @@ cp -r $SDK_MODULE_FOLDER/lib ./node_modules/renex-sdk-ts
 npm run build
 cd $BASE_FOLDER
 mv $RENEX_MODULE_FOLDER/build $UI_FOLDER
+
+git add ui env/latest_commit.txt
+
+set +x
+
+echo -en "\n\n\n"
+printf "${YELLOW}%`tput cols`s"|tr ' ' '='
+echo -e "\r==== Git log details ${RESET}"
+git status
+printf "${YELLOW}%`tput cols`s${RESET}\n\n"|tr ' ' '='
+
+echo "Version built from the following modules:"
+echo -e "${GREEN}renex-js:${BRANCH}${RESET} [${YELLOW}${LATEST_RENEX_COMMIT}${RESET}] commited by ${YELLOW}${LATEST_RENEX_AUTHOR}${RESET}"
+echo -e "${GREEN}renex-sdk-ts:${BRANCH}${RESET} [${YELLOW}${LATEST_SDK_COMMIT}${RESET}] commited by ${YELLOW}${LATEST_SDK_AUTHOR}${RESET}"
+echo ""
+
+if [ "$NETWORK" == "mainnet" ] || [ "$NOVERIFY" == false ]; then
+    echo -en "Re-enter network name (${COLOR}${NETWORK}${RESET}) to confirm deploy: ${COLOR}"
+    read CONFIRM
+    echo -en "${RESET}"
+    if [ "${CONFIRM}" != "${NETWORK}" ]; then
+        echo -e "${RED}Mismatched network.${RESET}"
+        exit 1
+    fi
+fi
+
+set -x
+
+git commit -m "ui: built ${COMBINED_HASH}" --no-verify
+
+git push
+git push "${NETWORK}" master
+
+set +x
+
+echo -e "\nPushed to ${COLOR}${NETWORK}${RESET}\n"
