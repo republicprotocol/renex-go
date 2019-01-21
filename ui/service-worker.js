1c3d39d6474c6d53d5e4ec95c87805ecbf32e95a
Negaex <gh@negaex.com>
2019-01-21 15:44:02 +1100

    ui: built c762a62//00d5313

--- ui/service-worker.js
+++ ui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3435823ba3b77443064d083f03106d14.js"
+  "/precache-manifest.29dfc9ff86fde5de7e581a6b9197ca75.js"
 );
 
 workbox.clientsClaim();
